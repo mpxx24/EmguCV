309f2b8f6c55cfa31555ff73685da50b72497dca
mpxx24 <mpxx24@gmail.com>
2015-01-17 12:51:02 +0100

    histogram

---
+++ projekt ECV/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace projekt_ECV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.histogramBox = new Emgu.CV.UI.HistogramBox();
+            this.pictureList = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(382, 40);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(259, 230);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // histogramBox
+            // 
+            this.histogramBox.Location = new System.Drawing.Point(56, 40);
+            this.histogramBox.Name = "histogramBox";
+            this.histogramBox.Size = new System.Drawing.Size(270, 230);
+            this.histogramBox.TabIndex = 3;
+            // 
+            // pictureList
+            // 
+            this.pictureList.FormattingEnabled = true;
+            this.pictureList.Location = new System.Drawing.Point(760, 40);
+            this.pictureList.Name = "pictureList";
+            this.pictureList.Size = new System.Drawing.Size(233, 225);
+            this.pictureList.TabIndex = 4;
+            this.pictureList.SelectedIndexChanged += new System.EventHandler(this.pictureList_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 327);
+            this.Controls.Add(this.pictureList);
+            this.Controls.Add(this.histogramBox);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Projekt EmguCV";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private Emgu.CV.UI.HistogramBox histogramBox;
+        private System.Windows.Forms.ListBox pictureList;
+    }
+}
+
---
+++ projekt ECV/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using ZedGraph;
+
+namespace projekt_ECV
+{
+    public partial class Form1 : Form
+    {
+        
+        private const string SciezkaFolderZeZdjeciami = @"C:\Users\Mariusz\Desktop\prAG\";
+        public Form1()
+        {
+            InitializeComponent();
+
+            var dInfo = new DirectoryInfo(SciezkaFolderZeZdjeciami);
+            var pliki = dInfo.GetFiles();
+            foreach (var plik in pliki)
+            {
+                pictureList.Items.Add(plik);
+            }
+        }
+        
+        private void pictureList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var zdjecie = pictureList.SelectedItem.ToString();
+            var pelnaSciezka = SciezkaFolderZeZdjeciami + zdjecie;
+            var image = Image.FromFile(pelnaSciezka);
+            var histogramImage = new Image<Bgr, Byte>(pelnaSciezka);
+
+            pictureBox.Image = image;
+            histogramBox.ClearHistogram();
+            histogramBox.GenerateHistograms(histogramImage, 256);
+            histogramBox.Refresh();
+        }
+    }
+}
